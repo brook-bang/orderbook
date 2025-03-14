f94e103010b1d9d0be3faea7c38dcdf9e686f8a2
brook-bang <1455047806@qq.com>
2025-03-14 17:10:47 +0800

    finish orderbook1

--- walac_orderbook/src/lib.rs
+++ walac_orderbook/src/lib.rs
@@ -169,14 +169,43 @@ impl OrderBook {
             let other_top = other_top.unwrap();
             let crossed = match top.side {
                 Side::Sell => other_top.price >= order.price,
-                Side::Buy => order.price>=other_top.price,
+                Side::Buy => order.price >= other_top.price,
             };
             if crossed {
-                
+                order_book.log.push(LogEntry::Reject {
+                    user_id: order.user_id,
+                    order_id: order.order_id,
+                });
+                return;
             }
         }
 
+        order_book.log.push(LogEntry::Acknowledge {
+            user_id: order.user_id,
+            order_id: order.order_id,
+        });
 
+        self.index
+            .insert((order.user_id, order.order_id), (symbol.to_owned(), *order));
+        order_book.orders.insert(*order);
+        let new_top = self.top(order.side, symbol);
+        self.log_top_of_book(symbol, top, new_top);
+    }
+
+    pub fn cancel(&mut self, user_id: usize, order_id: usize) {
+        match self.index.remove(&(user_id, order_id)) {
+            Some((symbol, order)) => {
+                let old_top = self.top(order.side, &symbol);
+                let order_book = self.order_book.get_mut(&symbol).unwrap();
+                order_book.orders.remove(&order);
+                order_book
+                    .log
+                    .push(LogEntry::Acknowledge { user_id, order_id });
+                let new_top = self.top(order.side, &symbol);
+                self.log_top_of_book(&symbol, old_top, new_top);
+            }
+            None => (),
+        }
     }
 
     pub fn top(&self, side: Side, symbol: &str) -> Option<Order> {
@@ -212,6 +241,13 @@ impl OrderBook {
         }
     }
 
+    pub fn get_logs(&self, symbol: &str) -> Option<&Vec<LogEntry>> {
+        match self.order_book.get(symbol) {
+            None => None,
+            Some(order_entry) => Some(&order_entry.log),
+        }
+    }
+
     fn total_volume<'a>(
         &self,
         it: impl Iterator<Item = &'a Order>,
@@ -228,5 +264,55 @@ impl OrderBook {
                 acc + x.volume
             });
         (total, min_order_id)
+    }
+
+    fn log_top_of_book(&mut self, symbol: &str, old_top: Option<Order>, new_top: Option<Order>) {
+        let order_book = self.order_book.get_mut(symbol).unwrap();
+        if new_top.is_none() {
+            order_book.log.push(LogEntry::TopOfBook {
+                side: None,
+                price: 0,
+                volume: 0,
+            });
+        } else if old_top.is_none() || old_top.unwrap() != new_top.unwrap() {
+            let order = new_top.unwrap();
+            order_book.log.push(LogEntry::TopOfBook {
+                side: Some(order.side),
+                price: order.price,
+                volume: order.volume,
+            });
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_side() {
+        assert_eq!(Side::new('B'), Some(Side::Buy));
+        assert_eq!(Side::new('S'), Some(Side::Sell));
+        assert_eq!(Side::new('T'), None);
+        assert_eq!(Side::from('B'), Side::Buy);
+        assert_eq!(Side::from('S'), Side::Sell);
+        assert_eq!(format!("{}", Side::Buy), "B".to_owned());
+        assert_eq!(format!("{}", Side::Sell), "S".to_owned());
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_side_invalid() {
+        let _ = Side::from('T');
+    }
+
+    #[test]
+    fn test_order_compare() {
+        assert!(Order::new(Side::Sell, 0, 0, 0, 0) < Order::new(Side::Buy, 0, 0, 0, 0));
+        assert!(Order::new(Side::Buy, 0, 0, 0, 0) > Order::new(Side::Sell, 0, 0, 0, 0));
+        assert!(Order::new(Side::Buy, 0, 0, 100, 20) > Order::new(Side::Buy, 0, 1, 100, 20));
+        assert!(Order::new(Side::Sell, 0, 0, 100, 20) < Order::new(Side::Sell, 0, 1, 100, 20));
+        assert!(Order::new(Side::Buy, 0, 0, 100, 10) < Order::new(Side::Buy, 0, 0, 200, 5));
+        assert!(Order::new(Side::Buy, 0, 0, 200, 5) > Order::new(Side::Buy, 0, 0, 100, 10));
     }
 }
